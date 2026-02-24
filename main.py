57b63193d3985dd997295b062178a0ce73cf0f8b
Z3J3M3 <3506397128@qq.com>
2026-02-24 13:53:36 +0800

    Fix syntax error in CIFAR100 dataset loading

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR100(
-    root=DATASET_PATH, train=False, download=True transform=transform_test)
+    root=DATASET_PATH, train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=0)
 
