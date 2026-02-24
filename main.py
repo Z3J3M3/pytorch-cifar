7157e86546dc5ff456c02c0c24d193a230bbff6e
Z3J3M3 <3506397128@qq.com>
2026-02-24 13:52:17 +0800

    Enable dataset downloading for CIFAR100

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR100(
-    root=DATASET_PATH, train=True, download=False, transform=transform_train)
+    root=DATASET_PATH, train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR100(
-    root=DATASET_PATH, train=False, download=False, transform=transform_test)
+    root=DATASET_PATH, train=False, download=True transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=0)
 
